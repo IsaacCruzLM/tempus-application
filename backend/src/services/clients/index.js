86c969d5b5b57c2a08636639748ad16dadf97e81
Isaac Cruz <isaac.clm1@gmail.com>
2022-01-22 17:52:41 -0300

    Revert "feat: corrige register Clients e adiciona funçao de buscar todos clientes do BD"
    This reverts commit b1bdd9ca8f9bcde30b3e2f22c1e66b5ae44f13ca.

--- backend/src/services/clients/index.js
+++ backend/src/services/clients/index.js
@@ -1,14 +1,12 @@
 const registerClientFunction = require('./registerClient');
 const getAllClientsFunction = require('./getAllClients');
-const getAllClientsByUserFunction = require('./getAllClientsByUser');
 const getClientByIdFunction = require('./getClientById');
 const updateClientByIdFunction = require('./updateClientById');
 const deleteClientByIdFunction = require('./deleteClientById');
 
 module.exports = {
   registerClient: (client, userId) => registerClientFunction(client, userId),
-  getAllClients: () => getAllClientsFunction(),
-  getAllClientsByUser: (userId) => getAllClientsByUserFunction(userId),
+  getAllClients: (userId) => getAllClientsFunction(userId),
   getClientById: (id) => getClientByIdFunction(id),
   updateClientById: (update, id, userId) => updateClientByIdFunction(update, id, userId),
   deleteClientById: (id, userId) => deleteClientByIdFunction(id, userId),
