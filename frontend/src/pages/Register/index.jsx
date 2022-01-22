e63352a44a3fd702e3dfb9fcb3599037e43d4c41
Isaac Cruz <isaac.clm1@gmail.com>
2022-01-22 18:19:11 -0300

    feat: Adiciona estrtura da pagina de registro de usuario

--- frontend/src/pages/Register/index.jsx
+++ frontend/src/pages/Register/index.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import Container from './styles';
+
+import {
+  Container, Form, Title, InputContainer, Input, Button, Label,
+} from './styles';
 
 function Main() {
   return (
     <Container className="Container">
-      Main
+      <Form action="" method="GET">
+        <Title>
+          Registrar
+        </Title>
+
+        <InputContainer>
+          <Label htmlFor="user">
+            Insira seu Nome
+            <Input required type="text" name="nome" />
+          </Label>
+        </InputContainer>
+
+        <InputContainer>
+          <Label htmlFor="user">
+            Insira seu Email
+            <Input required type="email" name="email" />
+          </Label>
+        </InputContainer>
+
+        <InputContainer>
+          <Label htmlFor="password">
+            Insira sua Senha
+            <Input required type="password" name="password" />
+          </Label>
+        </InputContainer>
+
+        <Button type="submit">Registrar</Button>
+      </Form>
     </Container>
   );
 }
